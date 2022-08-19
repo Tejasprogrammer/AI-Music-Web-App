64b6a049ece9be80f95cbfe7b23501245e05e994
Tejasprogrammer <78012113+Tejasprogrammer@users.noreply.github.com>
2022-08-19 13:43:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,18 +2,21 @@ function setup()
 {
     canvas = createCanvas(600,530);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
 }
 
+song="";
 
+function preload()
+{
+    song=loadSound("music.mp3" , "music2.mp3");
+}
 
-
-
-
-
-
-
-
-
+function draw()
+{
+image(video, 0,0,600,530);
+}
 
 
 

aeb32040c5d27b708be0cba2556000d35973e671
Tejasprogrammer <78012113+Tejasprogrammer@users.noreply.github.com>
2022-08-29 14:45:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
+rightwristx=0;
+rightwristy=0;
+
+leftwristx=0;
+leftwristy=0;
+
+score_rightwrist=0;
+score_leftwrist=0;
+
 function setup()
 {
     canvas = createCanvas(600,530);
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose" , gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet is Initialized");
 }
 
 song="";
@@ -18,7 +35,27 @@ function draw()
 image(video, 0,0,600,530);
 }
 
+function gotPoses(results)
+{
+    if(results.length > 0 )
+    {
+        console.log(results);
+        score_rightwrist=results[0].pose.keypoints[10].score;
 
+        score_leftwrist=results[0].pose.keypoints[9].score;
+       
+        console.log("score_rightwrist ="+ score_rightwrist + "score_leftwrist = "+score_leftwrist);
 
+       rightwristx=results[0].pose.rightwrist.x;
+       rightwristy=results[0].pose.rightwrist.y;
+
+       console.log("rightwristx = "+ rightwristx + "rightwristy = "+rightwristy);
+
+       leftwristx=results[0].pose.leftwrist.x;
+       leftwristy=results[0].pose.leftwrist.y;
+
+       console.log("left_wristx="+leftwristx + "left_wristy="+ lefttwristy);
+    }
+}
 
 
